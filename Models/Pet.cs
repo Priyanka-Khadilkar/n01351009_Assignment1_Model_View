a5ed9ff2f717e03949deaa178bb100b8560c1307
Priyanka Khadilkar <49756361+Priyanka-Khadilkar@users.noreply.github.com>
2020-01-23 16:33:40 -0500

    Update Pet.cs

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-//Install  entity framework 6 on Tools > Manage Nuget Packages > Microsoft Entity Framework (ver 6.4)
 using System.Data.Entity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -15,15 +14,8 @@ namespace PetGrooming.Models
         [Key]
         public int PetID { get; set; }
         public string PetName { get; set; }
-        //weight is in kilograms (kg)
         public double PetWeight { get; set; }
         public string PetColor { get; set; }
         public string PetNotes { get; set; }
-
-        //Representing the Many in (One species to Many Pets)
-        
-        public int SpeciesID { get; set; }
-        [ForeignKey("SpeciesID")]
-        public virtual Species Species { get; set; }
     }
 }
